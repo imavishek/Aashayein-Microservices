da5fd82dacd14fc7424a8877a5da3a435122fe74
Avishek Das <avishek.akd@gmail.com>
2019-06-11 19:35:57 +0530

    Docker Implemented for config-server, eureka-server, zipkin-server, zuul-gateway, hystrix-dashboard. Also added docker compose file.

--- config-server/src/main/java/com/aashayein/config/configuration/ConfigServerApplication.java
+++ config-server/src/main/java/com/aashayein/config/configuration/ConfigServerApplication.java
@@ -12,10 +12,8 @@ package com.aashayein.config.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
